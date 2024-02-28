45d0cd964d57f76125b26a38a712d6e3605f3c74
TusharDev456 <tusharkaira@theprofessionalworld.com>
2024-02-28 16:09:09 +0530

    added project

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello I am from Node AWS!!")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on the port : ${PORT}`)
+})
