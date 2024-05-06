c9912caa15752bf6243828c5b36c9fb3600e5f1b
TusharDev456 <tusharkaira@theprofessionalworld.com>
2024-05-06 14:00:27 +0530

    added the message

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const PORT = 4000;
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello Neeraj Sir Gaad Doge!") 
-    res.send("haa lelo meri!!") 
+    res.send("Hello Neeraj Sir Gaad Doge!,haa lelo meri!!") 
+    // res.send("haa lelo meri!!") 
 })
 
 app.listen(PORT,()=>{
