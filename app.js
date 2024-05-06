f86f05a156dd3e2784f451783ed40d21d83ebb8d
TusharDev456 <tusharkaira@theprofessionalworld.com>
2024-05-06 16:31:03 +0530

    added the message

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import express from "express";
+import cors from 'cors'
 
 const PORT = 4000;
 const app = express();
+app.use(express.json())
+app.use(cors())
 
 app.get('/',(req,res)=>{
     const data = axios.get("'https://jsonplaceholder.typicode.com/todos/1'")
