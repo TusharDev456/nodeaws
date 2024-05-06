79d189855b9f0324f9985ed772fbd18edb6c6d65
TusharDev456 <tusharkaira@theprofessionalworld.com>
2024-05-06 13:51:19 +0530

    added the new server name

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 4000;
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello I am from Node AWS!!")
+    res.send("Hello I am from Node AWS Account!!")
 })
 
 app.listen(PORT,()=>{
