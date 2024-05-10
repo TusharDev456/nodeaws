8716c61448c01278504cdd43ba2b171b08794a30
TusharDev456 <tusharkaira@theprofessionalworld.com>
2024-05-10 12:25:34 +0530

    added react frontend

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 import axios from "axios";
 import express from "express";
 import cors from 'cors'
+import path from 'path'
 
 const PORT = 4000;
 const app = express();
 app.use(express.json())
 app.use(cors())
 
-app.get('/',async(req,res)=>{
-    try {
-        const response = await axios.get("http://jsonplaceholder.typicode.com/posts");
-        res.send(response.data);
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        res.status(500).send("Error fetching data");
-    }
-})
+app.use(express.static(path.resolve('./public')))
 
 app.listen(PORT,()=>{
     console.log(`Server is running on the port : ${PORT}`)
